05bee5a48e0ae1997aae637a6df009ac39a41849
Wilkes Santos <wilkes.lira@gmail.com>
2026-02-24 15:08:49 -0300

    add new migrations

--- src/PeladaPay.Infrastructure/Migrations/20260224145424_add-relation-group-players.Designer.cs
+++ src/PeladaPay.Infrastructure/Migrations/20260224145424_add-relation-group-players.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PeladaPay.Infrastructure.Data;
@@ -11,9 +12,11 @@ using PeladaPay.Infrastructure.Data;
 namespace PeladaPay.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224145424_add-relation-group-players")]
+    partial class addrelationgroupplayers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
