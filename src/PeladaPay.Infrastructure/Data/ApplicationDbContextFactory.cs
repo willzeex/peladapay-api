adc5f83a5e006a563d506b8d136b5a8ed9382644
Wilkes Santos <wilkes.lira@gmail.com>
2026-03-06 12:30:29 -0300

    fix connectio string

---
+++ src/PeladaPay.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PeladaPay.Infrastructure.Data;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+        if (string.IsNullOrEmpty(connectionString))
+            throw new InvalidOperationException("ConnectionStrings__DefaultConnection environment variable not found.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
